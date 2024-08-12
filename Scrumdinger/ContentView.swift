2a311e49a3bfc672bd576f80503b3f435fdf88f4
Hilal Safi <hilalsafi0@gmail.com>
2024-08-11 21:39:41 -0400

    Test message

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Hilal Safi on 2024-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Testing")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hilal Safi on 2024-08-11.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
